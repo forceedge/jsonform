f22496ca79b43360f47d206af3d5d338c23ac5aa
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-05-12 11:13:03 +0100

    select tab

--- sharepoint.php
+++ sharepoint.php
@@ -9,7 +9,12 @@
     <?php require 'menu.php'; ?>
     <div class="container">
       <h1>Sharepoint UV Generator</h1>
-      <form></form>
+      <form>
+        <select id="pointType">
+          <option value="sharepoint">Sharepoint</option>
+          <option value="touchpoint">Touchpoint</option>
+        </select>
+      </form>
       <div id="res" class="alert"></div>
       <script type="text/javascript" src="deps/jquery.min.js"></script>
       <script type="text/javascript" src="deps/underscore.js"></script>
