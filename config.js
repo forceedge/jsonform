846f900e3497df0e09fad75834ae8350173c7715
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-05-12 12:38:38 +0100

    All in one file, and now optimized

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var mainJSON;
 
+// Generate the main form
 $('form#main').jsonForm({
   schema: {          
     "_at": {
@@ -78,229 +79,237 @@ function arrayToProperObject(arr) {
   return eval('({' + reformattedMeta.slice(0, -1) + '})');
 }
 
-jQuery('.controls > input').on('change', function(){
-  jQuery('li.active .draggable.tab').val(jQuery('.controls > input').val());
+// Hide the genrated forms
+jQuery('form.generated-json').hide();
+
+// Generate hidden forms
+// generate the rest of the form here
+$('form#sharepoint-generate-json').jsonForm({
+  schema: {          
+    "_at": {
+      type: "object",
+      title: "Sharepoint Data",
+      properties: {
+        shareChannel: {
+          type: "string",
+          title: "Share Channel",
+          default: '{ shareChannel }'
+        },
+        sharepointName: {
+          type: 'string',
+          title: 'Sharepoint Name',
+          required: true
+        },
+        facebookId: {
+          type: 'string',
+          title: 'Facebook ID',
+          default: '{ user.facebookId }'
+        },
+        twitterId: {
+          type: 'string',
+          title: 'Twitter ID',
+          default: '{ user.twitterId }'
+        },
+      },
+    },
+    "meta": {
+      type: "array",
+      items: {
+        type: "object",
+        title: "meta",
+        properties: {
+          "property": {
+            type: "string",
+            title: "Property {{idx}}"
+          },
+          "value": {
+            type: "array",
+            title: "Value {{idx}}",
+            items: {
+              type: "object",
+              properties: {
+                "property": {
+                  type: "string",
+                  title: "Property {{idx}}"
+                },
+                "value": {
+                  type: "string",
+                  title: "Value {{idx}}"
+                },
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  "form": [
+    {
+      "key": "_at"
+    },
+    {
+      "type": "tabarray",
+      "items": [{
+        "type": "section",
+        "legend": "meta {{idx}}",
+        "items": [
+          {
+            "key": "meta[].property",
+            "title": "Property"
+          },
+          {
+            "key": "meta[].value",
+            "title": "Value"
+          }
+        ],
+      }],
+    },
+    {
+      "type": "submit",
+      "value": "Generate"
+    }
+  ],
+  onSubmit: function (errors, values) {
+  jQuery('form#main').submit();
+
+  if(! mainJSON)
+    return false;
+
+  if(typeof values.meta !== 'undefined') {
+      values.meta = arrayToProperObject(values.meta);
+
+      for(var propertyName in values.meta) {
+        if(typeof values.meta[propertyName] !== 'undefined') {
+          values.meta[propertyName] = arrayToProperObject(values.meta[propertyName]);
+        }
+      };
+    }
+
+    if (errors) {
+      $('#res').html('<p>I beg your pardon?</p>');
+    }
+    else {
+      if (typeof values != 'string') {
+        values = $.extend(true, {}, mainJSON, values);
+        json = JSON.stringify(values, undefined, 2).replace(/[\r\n]/g, '<br />');
+      }
+
+      $('#res').html('<pre>window.advocate_things_data = '+ json +'</pre>');
+    }
+  }
 });
 
-jQuery("select[name='_at.pointType']").on('change', function() {
-  jQuery('form#generate-json').empty();
-  if($(this).val() == 'sharepoint') {
-    // generate the rest of the form here
-    $('form#generate-json').jsonForm({
-      schema: {          
-        "_at": {
-          type: "object",
-          title: "Sharepoint Data",
-          properties: {
-            shareChannel: {
-              type: "string",
-              title: "Share Channel",
-              default: '{ shareChannel }'
-            },
-            sharepointName: {
-              type: 'string',
-              title: 'Sharepoint Name',
-              required: true
-            },
-            facebookId: {
-              type: 'string',
-              title: 'Facebook ID',
-              default: '{ user.facebookId }'
-            },
-            twitterId: {
-              type: 'string',
-              title: 'Twitter ID',
-              default: '{ user.twitterId }'
-            },
+// Generate touchpoint form here
+// generate the rest of the form here
+$('form#touchpoint-generate-json').jsonForm({
+  schema: {          
+    "_at": {
+      type: "object",
+      title: "Touchpoint Data",
+      properties: {
+        TouchpointName: {
+          type: 'string',
+          title: 'Touchpoint Name',
+          required: true
+        }
+      }
+    },
+    "meta": {
+      type: "array",
+      items: {
+        type: "object",
+        title: "meta",
+        properties: {
+          "property": {
+            type: "string",
+            title: "Property {{idx}}"
           },
-        },
-        "meta": {
-          type: "array",
-          items: {
-            type: "object",
-            title: "meta",
-            properties: {
-              "property": {
-                type: "string",
-                title: "Property {{idx}}"
-              },
-              "value": {
-                type: "array",
-                title: "Value {{idx}}",
-                items: {
-                  type: "object",
-                  properties: {
-                    "property": {
-                      type: "string",
-                      title: "Property {{idx}}"
-                    },
-                    "value": {
-                      type: "string",
-                      title: "Value {{idx}}"
-                    },
-                  }
-                }
+          "value": {
+            type: "array",
+            title: "Value {{idx}}",
+            items: {
+              type: "object",
+              properties: {
+                "property": {
+                  type: "string",
+                  title: "Property {{idx}}"
+                },
+                "value": {
+                  type: "string",
+                  title: "Value {{idx}}"
+                },
               }
             }
           }
-        }
-      },
-      "form": [
-        {
-          "key": "_at"
-        },
-        {
-          "type": "tabarray",
-          "items": [{
-            "type": "section",
-            "legend": "meta {{idx}}",
-            "items": [
-              {
-                "key": "meta[].property",
-                "title": "Property"
-              },
-              {
-                "key": "meta[].value",
-                "title": "Value"
-              }
-            ],
-          }],
-        },
-        {
-          "type": "submit",
-          "value": "Generate"
-        }
-      ],
-      onSubmit: function (errors, values) {
-      jQuery('form#main').submit();
-
-      if(! mainJSON)
-        return false;
-
-      if(typeof values.meta !== 'undefined') {
-          values.meta = arrayToProperObject(values.meta);
-
-          for(var propertyName in values.meta) {
-            if(typeof values.meta[propertyName] !== 'undefined') {
-              values.meta[propertyName] = arrayToProperObject(values.meta[propertyName]);
-            }
-          };
-        }
-
-        if (errors) {
-          $('#res').html('<p>I beg your pardon?</p>');
-        }
-        else {
-          if (typeof values != 'string') {
-            values = $.extend(true, {}, mainJSON, values);
-            json = JSON.stringify(values, undefined, 2).replace(/[\r\n]/g, '<br />');
-          }
-
-          $('#res').html('<pre>window.advocate_things_data = '+ json +'</pre>');
         }
       }
-    });
-
-  } else if($(this).val() === 'touchpoint') {
-    // generate the rest of the form here
-    $('form#generate-json').jsonForm({
-      schema: {          
-        "_at": {
-          type: "object",
-          title: "Touchpoint Data",
-          properties: {
-            TouchpointName: {
-              type: 'string',
-              title: 'Touchpoint Name',
-              required: true
-            }
+    }
+  },
+  "form": [
+    {
+      "key": "_at"
+    },
+    {
+      "type": "tabarray",
+      "items": [{
+        "type": "section",
+        "legend": "meta {{idx}}",
+        "items": [
+          {
+            "key": "meta[].property",
+            "title": "Property"
+          },
+          {
+            "key": "meta[].value",
+            "title": "Value"
           }
-        },
-        "meta": {
-          type: "array",
-          items: {
-            type: "object",
-            title: "meta",
-            properties: {
-              "property": {
-                type: "string",
-                title: "Property {{idx}}"
-              },
-              "value": {
-                type: "array",
-                title: "Value {{idx}}",
-                items: {
-                  type: "object",
-                  properties: {
-                    "property": {
-                      type: "string",
-                      title: "Property {{idx}}"
-                    },
-                    "value": {
-                      type: "string",
-                      title: "Value {{idx}}"
-                    },
-                  }
-                }
-              }
-            }
-          }
-        }
-      },
-      "form": [
-        {
-          "key": "_at"
-        },
-        {
-          "type": "tabarray",
-          "items": [{
-            "type": "section",
-            "legend": "meta {{idx}}",
-            "items": [
-              {
-                "key": "meta[].property",
-                "title": "Property"
-              },
-              {
-                "key": "meta[].value",
-                "title": "Value"
-              }
-            ],
-          }],
-        },
-        {
-          "type": "submit",
-          "value": "Generate"
-        }
-      ],
-      onSubmit: function (errors, values) {
-      jQuery('form#main').submit();
+        ],
+      }],
+    },
+    {
+      "type": "submit",
+      "value": "Generate"
+    }
+  ],
+  onSubmit: function (errors, values) {
+  jQuery('form#main').submit();
 
-      if(! mainJSON)
-        return false;
+  if(! mainJSON)
+    return false;
 
-      if(typeof values.meta !== 'undefined') {
-          values.meta = arrayToProperObject(values.meta);
+  if(typeof values.meta !== 'undefined') {
+      values.meta = arrayToProperObject(values.meta);
 
-          for(var propertyName in values.meta) {
-            if(typeof values.meta[propertyName] !== 'undefined') {
-              values.meta[propertyName] = arrayToProperObject(values.meta[propertyName]);
-            }
-          };
+      for(var propertyName in values.meta) {
+        if(typeof values.meta[propertyName] !== 'undefined') {
+          values.meta[propertyName] = arrayToProperObject(values.meta[propertyName]);
         }
+      };
+    }
 
-        if (errors) {
-          $('#res').html('<p>I beg your pardon?</p>');
-        }
-        else {
-          if (typeof values != 'string') {
-            values = $.extend(true, {}, mainJSON, values);
-            json = JSON.stringify(values, undefined, 2).replace(/[\r\n]/g, '<br />');
-          }
-
-          $('#res').html('<pre>window.advocate_things_data = '+ json +'</pre>');
-        }
+    if (errors) {
+      $('#res').html('<p>I beg your pardon?</p>');
+    }
+    else {
+      if (typeof values != 'string') {
+        values = $.extend(true, {}, mainJSON, values);
+        json = JSON.stringify(values, undefined, 2).replace(/[\r\n]/g, '<br />');
       }
-    });
+
+      $('#res').html('<pre>window.advocate_things_data = '+ json +'</pre>');
+    }
+  }
+});
+
+// Switching between forms
+jQuery("select[name='_at.pointType']").on('change', function() {
+  // Reset stuff
+  jQuery('form.generated-json').hide();
+  jQuery('#res').empty();
+
+  if($(this).val() == 'sharepoint') {
+    jQuery('form#sharepoint-generate-json').show();
+  } else if($(this).val() === 'touchpoint') {    
+    jQuery('form#touchpoint-generate-json').show();
   }
 });
